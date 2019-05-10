04b6d764cd5ee77133bd89f157b694ac671388e8
Carles1983 <soane1983@163.com>
2019-05-10 16:58:49 +0800

    Create RoleRepository.java

---
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.ai.spring.cloud.oauth.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ai.spring.cloud.oauth.mongo.document.RoleDoc;
+
+public interface RoleRepository extends MongoRepository<RoleDoc, String> {
+
+}
