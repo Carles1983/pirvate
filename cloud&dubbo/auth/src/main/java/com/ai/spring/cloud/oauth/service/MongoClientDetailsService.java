f801ebd40e7a61d210772380d15069ed5e7e734a
Carles1983 <soane1983@163.com>
2019-05-10 17:20:00 +0800

    Create MongoClientDetailsService.java

---
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/service/MongoClientDetailsService.java
@@ -0,0 +1,15 @@
+package com.ai.spring.cloud.oauth.service;
+
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+
+public class MongoClientDetailsService implements ClientDetailsService {
+
+	@Override
+	public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
+		
+		return null;
+	}
+
+}
