602cad65bfca1a2a7cdc8ca5cffb447f99bdd0ed
Carles1983 <soane1983@163.com>
2019-05-10 16:58:51 +0800

    Create UserRepository.java

---
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ai.spring.cloud.oauth.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ai.spring.cloud.oauth.mongo.document.UserDoc;
+
+public interface UserRepository extends MongoRepository<UserDoc, String> {
+
+}
