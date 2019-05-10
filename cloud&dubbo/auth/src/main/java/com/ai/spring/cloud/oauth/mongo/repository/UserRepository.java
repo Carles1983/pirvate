4dfdadab367cc1ae52ec8e324f3f943eb5950ea5
Carles1983 <soane1983@163.com>
2019-05-10 17:14:27 +0800

    Update UserRepository.java

--- cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/repository/UserRepository.java
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.ai.spring.cloud.oauth.mongo.document.UserDoc;
 
 public interface UserRepository extends MongoRepository<UserDoc, String> {
-
+	UserDoc findUserByName(String username);
 }
