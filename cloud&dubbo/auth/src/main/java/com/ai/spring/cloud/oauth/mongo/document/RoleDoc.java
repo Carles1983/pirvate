3917cb9b3b64a34fb219f3d2770850f3b3797b58
Carles1983 <soane1983@163.com>
2019-05-10 17:14:23 +0800

    Update RoleDoc.java

--- cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/document/RoleDoc.java
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/document/RoleDoc.java
@@ -1,5 +1,19 @@
 package com.ai.spring.cloud.oauth.mongo.document;
 
-public class RoleDoc {
+import java.util.Date;
+import java.util.Set;
 
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+@Data
+public class RoleDoc {
+	@Id
+	private String id;
+    private String name;
+    private Integer valid;
+    private Date createTime;
+    private Date updateTime;
+    private Set<PermissionDoc> permissions;
 }
