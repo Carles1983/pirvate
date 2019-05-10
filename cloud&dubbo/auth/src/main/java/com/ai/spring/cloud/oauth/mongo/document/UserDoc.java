41ddd2743e6c7078cc4c58fb8d50bffe4c9a388e
Carles1983 <soane1983@163.com>
2019-05-10 17:03:03 +0800

    Update UserDoc.java

--- cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/document/UserDoc.java
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/document/UserDoc.java
@@ -1,5 +1,22 @@
 package com.ai.spring.cloud.oauth.mongo.document;
 
-public class UserDoc {
+import java.util.Date;
+import java.util.Set;
 
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+@Data
+public class UserDoc {
+	@Id
+	private String id;
+    private String memberName;
+    private String password;
+    private String mobile;
+    private String email;
+    private Integer sex;
+    private Date birthday;
+    private Date createTime;
+    private Set<RoleDoc> roles;
 }
