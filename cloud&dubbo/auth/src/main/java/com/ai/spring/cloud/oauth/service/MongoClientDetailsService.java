fe3be8b45557802f3b517a9c743f1f81480c152e
Carles1983 <soane1983@163.com>
2019-05-10 17:39:28 +0800

    Update MongoClientDetailsService.java

--- cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/service/MongoClientDetailsService.java
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/service/MongoClientDetailsService.java
@@ -1,15 +1,28 @@
 package com.ai.spring.cloud.oauth.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.stereotype.Service;
 
+import com.ai.spring.cloud.oauth.converter.ClientDetailsConverter;
+import com.ai.spring.cloud.oauth.mongo.document.ClientDetailDoc;
+import com.ai.spring.cloud.oauth.mongo.repository.ClientDetailRepository;
+
+@Service
 public class MongoClientDetailsService implements ClientDetailsService {
 
+	@Autowired
+	private ClientDetailRepository clientDetailReository;
+	
 	@Override
 	public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
-		
-		return null;
+		Optional<ClientDetailDoc> clientDoc = clientDetailReository.findById(clientId);
+		ClientDetailDoc doc = clientDoc.get();
+		return ClientDetailsConverter.convert(doc);
 	}
 
 }
