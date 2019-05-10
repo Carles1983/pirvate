39cbdef219966546194df28c7d933142b7bcda88
Carles1983 <soane1983@163.com>
2019-05-10 16:14:59 +0800

    Create NoEncryptPasswordEncoder.java

---
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/config/NoEncryptPasswordEncoder.java
@@ -0,0 +1,20 @@
+package com.ai.spring.cloud.oauth.config;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class NoEncryptPasswordEncoder implements PasswordEncoder {
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return (String) rawPassword;
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		//密码对比 密码对 true 反之 false
+        //rawPassword 数据库中的密码
+        //encodedPassword 前台传入的密码
+        return encodedPassword.equals((String) rawPassword);
+	}
+
+}
