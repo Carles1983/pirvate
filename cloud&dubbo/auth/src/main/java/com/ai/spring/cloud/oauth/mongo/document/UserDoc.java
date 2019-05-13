9a1a6911a82aff825a9dba07c51510afd0016ae6
Carles1983 <soane1983@163.com>
2019-05-13 10:51:23 +0800

    Update UserDoc.java

--- cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/document/UserDoc.java
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/document/UserDoc.java
@@ -11,7 +11,7 @@ import lombok.Data;
 public class UserDoc {
 	@Id
 	private String id;
-    private String Name;
+    private String name;
     private String password;
     private String mobile;
     private String email;
