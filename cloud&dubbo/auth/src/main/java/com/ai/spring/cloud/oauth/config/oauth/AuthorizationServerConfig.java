dd32db334cdea3d788c2fbea98bbdc6773c9cab2
Carles1983 <soane1983@163.com>
2019-05-13 09:10:49 +0800

    Update AuthorizationServerConfig.java

--- cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/config/oauth/AuthorizationServerConfig.java
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/config/oauth/AuthorizationServerConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
