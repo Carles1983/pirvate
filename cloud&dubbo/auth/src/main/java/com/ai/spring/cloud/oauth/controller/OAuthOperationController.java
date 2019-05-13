8bbd8f3a3eb6981c6bf7f6d2cd7274330979402d
Carles1983 <soane1983@163.com>
2019-05-13 10:51:21 +0800

    Update OAuthOperationController.java

--- cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/controller/OAuthOperationController.java
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/controller/OAuthOperationController.java
@@ -1,9 +1,14 @@
 package com.ai.spring.cloud.oauth.controller;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ai.spring.cloud.oauth.mongo.document.ClientDetailDoc;
@@ -39,12 +44,20 @@ public class OAuthOperationController {
 	
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
 	public String addUser(@RequestBody UserDoc userDoc) {
+		userDoc.setCreateTime(new Date(System.currentTimeMillis()));
+		RoleDoc role1 = roleRepository.findByName("role1");
+		RoleDoc role2 = roleRepository.findByName("role2");
+		Set<RoleDoc> roleSet = new HashSet<>();
+		roleSet.add(role1);
+		roleSet.add(role2);
+		userDoc.setRoles(roleSet);
 		userRepository.save(userDoc);
 		return "success";
 	}
 	
 	@RequestMapping(value = "/role", method = RequestMethod.POST)
 	public String addRole(@RequestBody RoleDoc roleDoc) {
+		roleDoc.setCreateTime(new Date(System.currentTimeMillis()));
 		roleRepository.save(roleDoc);
 		return "success";
 	}
@@ -53,6 +66,12 @@ public class OAuthOperationController {
 	public String addPermission(@RequestBody PermissionDoc permissionDoc) {
 		permissionRepository.save(permissionDoc);
 		return "success";
+	}
+	
+	@RequestMapping(value = "/api/user", method = RequestMethod.GET)
+	public UserDoc addUser(@RequestParam String username) {
+		UserDoc doc = userRepository.findUserByName(username);
+		return doc;
 	}
 	
 }
