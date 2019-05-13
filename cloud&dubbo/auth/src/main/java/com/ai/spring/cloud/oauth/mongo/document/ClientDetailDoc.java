d1b8bde8103e06038bf07ea6080b5f1053131cc1
Carles1983 <soane1983@163.com>
2019-05-13 09:23:02 +0800

    Update ClientDetailDoc.java

--- cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/document/ClientDetailDoc.java
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/document/ClientDetailDoc.java
@@ -8,15 +8,15 @@ import lombok.Data;
 public class ClientDetailDoc {
 	@Id
 	private String id;
-	private String client_id;
-	private String resource_ids;
-	private String client_secret;
+	private String clientId;
+	private String resourceIds;
+	private String clientSecret;
 	private String scope;
-	private String authorized_grant_types;
-	private String web_server_redirect_uri;
+	private String authorizedGrantTypes;
+	private String webServerRedirectUri;
 	private String authorities;
-	private Integer access_token_validity;
-	private Integer refresh_token_validity;
-	private String additional_information;
+	private Integer accessTokenValidity;
+	private Integer refreshTokenValidity;
+	private String additionalInformation;
 	private String autoapprove;
 }
