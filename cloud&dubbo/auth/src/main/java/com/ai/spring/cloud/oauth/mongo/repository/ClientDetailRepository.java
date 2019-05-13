4386f60a8d71b92494994c71dced8f8a9eb58f21
Carles1983 <soane1983@163.com>
2019-05-13 09:10:56 +0800

    Update ClientDetailRepository.java

--- cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/repository/ClientDetailRepository.java
+++ cloud&dubbo/auth/src/main/java/com/ai/spring/cloud/oauth/mongo/repository/ClientDetailRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.ai.spring.cloud.oauth.mongo.document.ClientDetailDoc;
 
 public interface ClientDetailRepository extends MongoRepository<ClientDetailDoc, String> {
-
+	ClientDetailDoc findByClientId(String clientId);
 }
