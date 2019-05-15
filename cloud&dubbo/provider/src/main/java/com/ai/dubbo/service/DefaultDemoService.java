32f630b8260fefee2e7b35d73ad9254f684ea609
Carles1983 <soane1983@163.com>
2019-05-15 15:01:51 +0800

    Update DefaultDemoService.java

--- cloud&dubbo/provider/src/main/java/com/ai/dubbo/service/DefaultDemoService.java
+++ cloud&dubbo/provider/src/main/java/com/ai/dubbo/service/DefaultDemoService.java
@@ -2,6 +2,9 @@ package com.ai.dubbo.service;
 
 import com.ai.dubbo.api.DemoService;
 import com.alibaba.dubbo.config.annotation.Service;
+
+import org.apache.skywalking.apm.toolkit.trace.ActiveSpan;
+import org.apache.skywalking.apm.toolkit.trace.Trace;
 import org.springframework.beans.factory.annotation.Value;
 
 @Service
@@ -10,8 +13,11 @@ public class DefaultDemoService implements DemoService {
 	@Value("${dubbo.protocol.port}")
 	private int port;
 
+	@Trace
 	@Override
 	public String sayHello(String name) {
+		ActiveSpan.tag("provider-port", String.valueOf(port));
+		ActiveSpan.tag("provider-name", name);
 		return name + "---> dubbo port : " + port;
 	}
 }
