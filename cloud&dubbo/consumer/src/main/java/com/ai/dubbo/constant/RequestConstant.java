afcd09482be72e7701e9a9782a93d222109dfc31
Carles1983 <soane1983@163.com>
2019-05-16 18:08:53 +0800

    Create RequestConstant.java

---
+++ cloud&dubbo/consumer/src/main/java/com/ai/dubbo/constant/RequestConstant.java
@@ -0,0 +1,16 @@
+package com.ai.dubbo.constant;
+
+public class RequestConstant {
+	
+	private RequestConstant() {}
+	
+	public static final String REQUEST_HEADER_USER_KEY = "userId";
+	public static final String REQUEST_HEADER_TIME_KEY = "timeStamp";
+	public static final String REQUEST_HEADER_SIGNATURE_KEY = "signature";
+	
+	public static final String REQUEST_RTSS_USER_KEY = "RTSS_USER_";
+	
+	public static final String REQUEST_SIGNSECURITY_HKEY = "SignSecurityKey";
+	public static final String REQUEST_CONTSECURITY_HKEY = "ContSecurityKey";
+	public static final String REQUEST_USERCODE_HKEY = "UserCode";
+}
