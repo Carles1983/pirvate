c57b1a56f9022ad18e4f24d86783d5c5959c001e
Carles1983 <soane1983@163.com>
2019-05-17 17:47:34 +0800

    Update AgrAttachServiceImpl.java

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/register/AgrAttachServiceImpl.java
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/register/AgrAttachServiceImpl.java
@@ -1,9 +1,17 @@
 package com.aii.crm.agr.core.register;
 
 import com.aii.crm.agr.api.AgrAttachService;
+import com.aii.crm.agr.core.service.interfaces.IAgrAttachSV;
 import com.aii.crm.agr.persistence.AgrAttachDto;
+import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@Service
 public class AgrAttachServiceImpl implements AgrAttachService {
+
+	@Autowired
+	private IAgrAttachSV agrAttachSV;
+
 	@Override
 	public Long saveAgrAttach(AgrAttachDto agrAttachDto) throws Exception {
 		return null;
