d636b9a8c3397809daecdbb7c06df262f08d57ab
Carles1983 <soane1983@163.com>
2019-05-17 16:59:17 +0800

    Update AgrAttachService.java

--- crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/crm/agr/api/AgrAttachService.java
+++ crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/crm/agr/api/AgrAttachService.java
@@ -2,30 +2,31 @@ package com.aii.crm.agr.api;
 
 import com.aii.crm.agr.persistence.AgrDto;
 import com.aii.crm.agr.persistence.AgrTemplateDto;
+import com.aii.crm.common.exception.CrmCheckedException;
 import java.util.List;
 import java.util.Map;
 
 public interface AgrAttachService {
 
-	AgrTemplateDto queryAgrTemplateByOfferId(Long offerId) throws Exception;
+	AgrTemplateDto queryAgrTemplateByOfferId(Long offerId) throws CrmCheckedException;
 
-	Long saveAgrAgreement(AgrDto agreementDto) throws Exception;
+	Long saveAgrAgreement(AgrDto agreementDto) throws CrmCheckedException;
 
 	Long updateAgreementStatusByCustomerIdAndOrderId(Long customerId, Long customerOrderId,
-												  Long agreementStatus) throws Exception;
+												  Long agreementStatus) throws CrmCheckedException;
 
-	Long signAgreement(Long agreementId) throws Exception;
+	Long signAgreement(Long agreementId) throws CrmCheckedException;
 
-	byte[] getBytesForXDoc(String docUrl, Map<String, String> params) throws Exception;
+	byte[] getBytesForXDoc(String docUrl, Map<String, String> params) throws CrmCheckedException;
 
-	List<AgrDto> queryAgreementByCustIdAndCustOrderId(Long custId, Long custOrderId) throws Exception;
+	List<AgrDto> queryAgreementByCustIdAndCustOrderId(Long custId, Long custOrderId) throws CrmCheckedException;
 
-	Long confirmAgreement(List<Long> customerOrderIdList, Long customerId) throws Exception;
+	Long confirmAgreement(List<Long> customerOrderIdList, Long customerId) throws CrmCheckedException;
 
-	Long updateAgreement(AgrDto agreementDto) throws Exception;
+	Long updateAgreement(AgrDto agreementDto) throws CrmCheckedException;
 
-	Long deleteAgreement(Long agreementId) throws Exception;
+	Long deleteAgreement(Long agreementId) throws CrmCheckedException;
 
-	AgrDto queryAgreementByAgreementId(Long agreementId) throws Exception;
+	AgrDto queryAgreementByAgreementId(Long agreementId) throws CrmCheckedException;
 
 }
