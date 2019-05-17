3791bc108e7d6735daa1239d918c3e012f3b0272
Carles1983 <soane1983@163.com>
2019-05-17 17:20:42 +0800

    Update AgrAttachService.java

--- crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/crm/agr/api/AgrAttachService.java
+++ crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/crm/agr/api/AgrAttachService.java
@@ -1,32 +1,7 @@
 package com.aii.crm.agr.api;
 
-import com.aii.crm.agr.persistence.AgrDto;
-import com.aii.crm.agr.persistence.AgrTemplateDto;
-import com.aii.crm.common.exception.CrmCheckedException;
-import java.util.List;
-import java.util.Map;
+import com.aii.crm.agr.persistence.AgrAttachDto;
 
 public interface AgrAttachService {
-
-	AgrTemplateDto queryAgrTemplateByOfferId(Long offerId) throws CrmCheckedException;
-
-	Long saveAgrAgreement(AgrDto agreementDto) throws CrmCheckedException;
-
-	Long updateAgreementStatusByCustomerIdAndOrderId(Long customerId, Long customerOrderId,
-												  Long agreementStatus) throws CrmCheckedException;
-
-	Long signAgreement(Long agreementId) throws CrmCheckedException;
-
-	byte[] getBytesForXDoc(String docUrl, Map<String, String> params) throws CrmCheckedException;
-
-	List<AgrDto> queryAgreementByCustIdAndCustOrderId(Long custId, Long custOrderId) throws CrmCheckedException;
-
-	Long confirmAgreement(List<Long> customerOrderIdList, Long customerId) throws CrmCheckedException;
-
-	Long updateAgreement(AgrDto agreementDto) throws CrmCheckedException;
-
-	Long deleteAgreement(Long agreementId) throws CrmCheckedException;
-
-	AgrDto queryAgreementByAgreementId(Long agreementId) throws CrmCheckedException;
-
+	Long saveAgrAttach(AgrAttachDto agrAttachDto) throws Exception;
 }
