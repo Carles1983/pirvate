66a88f70c9f2d8564222e58578c34a3d16e281c1
Carles1983 <soane1983@163.com>
2019-05-17 17:20:32 +0800

    Create AgrAttachServiceImpl.java

---
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/register/AgrAttachServiceImpl.java
@@ -0,0 +1,11 @@
+package com.aii.crm.agr.core.register;
+
+import com.aii.crm.agr.api.AgrAttachService;
+import com.aii.crm.agr.persistence.AgrAttachDto;
+
+public class AgrAttachServiceImpl implements AgrAttachService {
+	@Override
+	public Long saveAgrAttach(AgrAttachDto agrAttachDto) throws Exception {
+		return null;
+	}
+}
