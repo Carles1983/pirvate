43eed96f71193b3eb996c383ce83e2148585d3c9
Carles1983 <soane1983@163.com>
2019-05-17 16:39:20 +0800

    Update AgrAttachService.java

--- crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/agr/api/AgrAttachService.java
+++ crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/agr/api/AgrAttachService.java
@@ -1,4 +1,31 @@
 package com.aii.agr.api;
 
+import com.aii.agr.persistence.AgrDto;
+import com.aii.agr.persistence.AgrTemplateDto;
+import java.util.List;
+import java.util.Map;
+
 public interface AgrAttachService {
+
+	AgrTemplateDto queryAgrTemplateByOfferId(Long offerId) throws Exception;
+
+	Long saveAgrAgreement(AgrDto agreementDto) throws Exception;
+
+	Long updateAgreementStatusByCustomerIdAndOrderId(Long customerId, Long customerOrderId,
+												  Long agreementStatus) throws Exception;
+
+	Long signAgreement(Long agreementId) throws Exception;
+
+	byte[] getBytesForXDoc(String docUrl, Map<String, String> params) throws Exception;
+
+	List<AgrDto> queryAgreementByCustIdAndCustOrderId(Long custId, Long custOrderId) throws Exception;
+
+	Long confirmAgreement(List<Long> customerOrderIdList, Long customerId) throws Exception;
+
+	Long updateAgreement(AgrDto agreementDto) throws Exception;
+
+	Long deleteAgreement(Long agreementId) throws Exception;
+
+	AgrDto queryAgreementByAgreementId(Long agreementId) throws Exception;
+
 }
