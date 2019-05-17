1a8b90a082b2159fb3634ae80cc7cd7d01a606b6
Carles1983 <soane1983@163.com>
2019-05-17 17:47:37 +0800

    Create AgrAttachSVImpl.java

---
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/impl/AgrAttachSVImpl.java
@@ -0,0 +1,8 @@
+package com.aii.crm.agr.core.service.impl;
+
+import com.aii.crm.agr.core.service.interfaces.IAgrAttachSV;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgrAttachSVImpl implements IAgrAttachSV {
+}
