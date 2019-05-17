4dffd29cb4f49c536b1643ab8dc6c085e0ff0462
Carles1983 <soane1983@163.com>
2019-05-17 16:33:10 +0800

    Update AgrDataDto.java

--- crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/agr/persistence/AgrDataDto.java
+++ crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/agr/persistence/AgrDataDto.java
@@ -1,4 +1,16 @@
 package com.aii.agr.persistence;
 
-public class AgrDataDto {
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+public class AgrDataDto implements Serializable {
+	private Long orderCustomerId;
+	private Long agreementId;
+	private String offerName;
+	private String agrUrl;
+	private Long custId;
+	private Integer custType;
+	private String agrSource;
+	private String message;
 }
