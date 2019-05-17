632b95d02dbc59db00e6fa2ea7927f4c485cd735
Carles1983 <soane1983@163.com>
2019-05-17 16:33:07 +0800

    Update AgrAttachDto.java

--- crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/agr/persistence/AgrAttachDto.java
+++ crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/agr/persistence/AgrAttachDto.java
@@ -1,4 +1,27 @@
 package com.aii.agr.persistence;
 
-public class AgrAttachDto {
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class AgrAttachDto implements Serializable {
+	private Long attachId;
+	private String busiTypeCode;
+	private String busiSheetId;
+	private String attachType;
+	private String fileName;
+	private String storageFileUri;
+	private String storeMediumType;
+	private String attachStatus;
+	private String remarks;
+	private Date agrCreationTime;
+	private Date createDate;
+	private Long createOpId;
+	private Long createOrgId;
+	private Long opId;
+	private Long orgId;
+	private Date doneDate;
+	private String doneCode;
+	private Long state;
 }
