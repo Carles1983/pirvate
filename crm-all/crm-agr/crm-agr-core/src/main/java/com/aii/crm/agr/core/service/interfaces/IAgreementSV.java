c2a295a43cf4784b5d55e9b5f3cb03705afcffe6
Carles1983 <soane1983@163.com>
2019-05-18 12:18:25 +0800

    Create IAgreementSV.java

---
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/interfaces/IAgreementSV.java
@@ -0,0 +1,7 @@
+package com.aii.crm.agr.core.service.interfaces;
+
+import com.aii.crm.agr.persistence.req.AgrReqDto;
+
+public interface IAgreementSV {
+	Long insertAgreement(AgrReqDto agrReqDto);
+}
