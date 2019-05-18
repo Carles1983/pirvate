d677eb094a4bb35f8c0bb45e21a441611f0ac0af
Carles1983 <soane1983@163.com>
2019-05-18 12:51:36 +0800

    Update AgreementSVImpl.java

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/impl/AgreementSVImpl.java
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/impl/AgreementSVImpl.java
@@ -1,9 +1,15 @@
 package com.aii.crm.agr.core.service.impl;
 
 import com.aii.crm.agr.core.persistence.bo.AgrAgreement;
+import com.aii.crm.agr.core.persistence.bo.AgrAgreementExample;
 import com.aii.crm.agr.core.persistence.mapper.AgrAgreementMapper;
 import com.aii.crm.agr.core.service.interfaces.IAgreementSV;
 import com.aii.crm.agr.persistence.req.AgrReqDto;
+import com.aii.crm.common.time.TimesUtil;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,5 +26,28 @@ public class AgreementSVImpl implements IAgreementSV {
 		BeanUtils.copyProperties(agrReqDto, agreement);
 		agrAgreementMapper.insertSelective(agreement);
 		return agreement.getAgreementId();
+	}
+
+	@Override
+	public List<AgrAgreement> queryAgreementByCustomerIdAndOrderId(Long customerId, Long customerOrderId, Long agrStateValid) {
+		AgrAgreementExample example = new AgrAgreementExample();
+		AgrAgreementExample.Criteria criteria = example.createCriteria();
+		criteria.andOwnerIdEqualTo(customerId);
+		criteria.andAgrSourceObjIdEqualTo(customerOrderId);
+		criteria.andAgreementStatusEqualTo(agrStateValid);
+		return agrAgreementMapper.selectByExample(example);
+	}
+
+	@Override
+	public Integer updateAgreementStatusBatch(List<AgrAgreement> agreementList, Long agreementStatus) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("agreementStatus", agreementStatus);
+		map.put("doneDate", TimesUtil.getDefaultTime());
+		List<Long> agreementIdList = new ArrayList<>();
+		for(AgrAgreement agrAgreement : agreementList){
+			agreementIdList.add(agrAgreement.getAgreementId());
+		}
+		map.put("agreementIdList", agreementIdList);
+		return agrAgreementMapper.updateStatusByBatchPrimaryKey(map);
 	}
 }
