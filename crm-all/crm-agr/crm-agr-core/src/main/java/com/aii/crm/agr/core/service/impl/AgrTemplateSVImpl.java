87fa79dfddd13a6cfcd9e5e687371c80760278bd
Carles1983 <soane1983@163.com>
2019-05-18 12:11:38 +0800

    Update AgrTemplateSVImpl.java

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/impl/AgrTemplateSVImpl.java
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/impl/AgrTemplateSVImpl.java
@@ -1,7 +1,8 @@
 package com.aii.crm.agr.core.service.impl;
 
 import com.aii.crm.agr.core.persistence.bo.AgrTemplate;
-import com.aii.crm.agr.core.persistence.mapper.AgrTemplateHMapper;
+import com.aii.crm.agr.core.persistence.bo.AgrTemplateExample;
+import com.aii.crm.agr.core.persistence.mapper.AgrTemplateMapper;
 import com.aii.crm.agr.core.service.interfaces.IAgrTemplateSV;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,13 @@ import org.springframework.stereotype.Service;
 public class AgrTemplateSVImpl implements IAgrTemplateSV {
 
 	@Autowired
-	private AgrTemplateHMapper agrTemplateHMapper;
+	private AgrTemplateMapper agrTemplateMapper;
 
 	@Override
 	public List<AgrTemplate> listAgrTempalteByOfferId(Long offerId) {
-		return null;
+		AgrTemplateExample example = new AgrTemplateExample();
+		AgrTemplateExample.Criteria criteria = example.createCriteria();
+		criteria.andOfferIdEqualTo(offerId);
+		return agrTemplateMapper.selectByExample(example);
 	}
 }
