92469faba9e325a7b3c2df4973c8bfc1deaf4a58
Carles1983 <soane1983@163.com>
2019-05-18 13:34:46 +0800

    Update AgreementSVImpl.java

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/impl/AgreementSVImpl.java
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/impl/AgreementSVImpl.java
@@ -1,18 +1,18 @@
 package com.aii.crm.agr.core.service.impl;
 
+import com.aii.crm.agr.core.constant.AgrConstant;
 import com.aii.crm.agr.core.persistence.bo.AgrAgreement;
 import com.aii.crm.agr.core.persistence.bo.AgrAgreementExample;
 import com.aii.crm.agr.core.persistence.mapper.AgrAgreementMapper;
 import com.aii.crm.agr.core.service.interfaces.IAgreementSV;
-import com.aii.crm.agr.persistence.req.AgrReqDto;
 import com.aii.crm.common.time.TimesUtil;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 @Service
 public class AgreementSVImpl implements IAgreementSV {
@@ -21,15 +21,14 @@ public class AgreementSVImpl implements IAgreementSV {
 	private AgrAgreementMapper agrAgreementMapper;
 
 	@Override
-	public Long insertAgreement(AgrReqDto agrReqDto) {
-		AgrAgreement agreement = new AgrAgreement();
-		BeanUtils.copyProperties(agrReqDto, agreement);
+	public Long insertAgreement(AgrAgreement agreement) {
 		agrAgreementMapper.insertSelective(agreement);
 		return agreement.getAgreementId();
 	}
 
 	@Override
-	public List<AgrAgreement> queryAgreementByCustomerIdAndOrderId(Long customerId, Long customerOrderId, Long agrStateValid) {
+	public List<AgrAgreement> queryAgreementByCustIdAndCustOrderId(Long customerId, Long customerOrderId,
+																Long agrStateValid) {
 		AgrAgreementExample example = new AgrAgreementExample();
 		AgrAgreementExample.Criteria criteria = example.createCriteria();
 		criteria.andOwnerIdEqualTo(customerId);
@@ -38,8 +37,7 @@ public class AgreementSVImpl implements IAgreementSV {
 		return agrAgreementMapper.selectByExample(example);
 	}
 
-	@Override
-	public Integer updateAgreementStatusBatch(List<AgrAgreement> agreementList, Long agreementStatus) {
+	private Integer updateAgreementStatusBatch(List<AgrAgreement> agreementList, Long agreementStatus) {
 		Map<String, Object> map = new HashMap<>();
 		map.put("agreementStatus", agreementStatus);
 		map.put("doneDate", TimesUtil.getDefaultTime());
@@ -52,8 +50,38 @@ public class AgreementSVImpl implements IAgreementSV {
 	}
 
 	@Override
-	public Long signAgreement(Long agreementId) {
-
-		return null;
+	public Integer agreementStatusChange(Long agreementId, Long agrStatusSign) {
+		AgrAgreement agreement = agrAgreementMapper.selectByPrimaryKey(agreementId);
+		if(agreement == null){
+			return null;
+		}
+		agreement.setAgreementStatus(agrStatusSign);
+		return agrAgreementMapper.updateByPrimaryKeySelective(agreement);
 	}
+
+	@Override
+	public Integer updateAgreementStatusByCustomerIdAndOrderId(Long customerId, Long customerOrderId, Long agreementStatus) {
+		List<AgrAgreement> agreementList = queryAgreementByCustIdAndCustOrderId(customerId,
+				customerOrderId, AgrConstant.AGR_STATE_VALID);
+		if (!CollectionUtils.isEmpty(agreementList)) {
+			return updateAgreementStatusBatch(agreementList, agreementStatus);
+		}
+		return 0;
+	}
+
+	@Override
+	public Integer updateAgreementStatusByCustomerIdListAndOrderId(Long customerId, List<Long> customerOrderIdList,
+																   Long agrStatusConfirm) {
+		AgrAgreementExample example = new AgrAgreementExample();
+		AgrAgreementExample.Criteria criteria = example.createCriteria();
+		criteria.andOwnerIdEqualTo(customerId);
+		criteria.andAgrSourceObjIdIn(customerOrderIdList);
+		List<AgrAgreement> agreementList = agrAgreementMapper.selectByExample(example);
+		if (!CollectionUtils.isEmpty(agreementList)) {
+			return updateAgreementStatusBatch(agreementList, agrStatusConfirm);
+		}
+		return 0;
+	}
+
+
 }
