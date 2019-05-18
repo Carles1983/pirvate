d9fc0141a5389482c23e22a8c345785ffdbfe6fa
Carles1983 <soane1983@163.com>
2019-05-18 11:36:29 +0800

    Create BeanConvertUtil.java

---
+++ crm-all/crm-common/src/main/java/com/aii/crm/common/bean/BeanConvertUtil.java
@@ -0,0 +1,26 @@
+package com.aii.crm.common.bean;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.collections4.CollectionUtils;
+
+public class BeanConvertUtil {
+
+	private BeanConvertUtil() {
+	}
+
+	public static <T, E> List<T> listConversion(List<E> beanList, Class<T> clazz) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+		List<T> resultList = new ArrayList<>();
+		if (CollectionUtils.isNotEmpty(beanList)) {
+			for (E e : beanList) {
+				T t = clazz.newInstance();
+				BeanUtils.copyProperties(e, t);
+				resultList.add(t);
+			}
+		}
+		return resultList;
+	}
+
+}
