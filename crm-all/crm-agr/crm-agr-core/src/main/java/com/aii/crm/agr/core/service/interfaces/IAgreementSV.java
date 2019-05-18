b4c8723ad63c972a98f8a2b09cce04bfaf38a339
Carles1983 <soane1983@163.com>
2019-05-18 12:51:38 +0800

    Update IAgreementSV.java

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/interfaces/IAgreementSV.java
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/interfaces/IAgreementSV.java
@@ -1,7 +1,13 @@
 package com.aii.crm.agr.core.service.interfaces;
 
+import com.aii.crm.agr.core.persistence.bo.AgrAgreement;
 import com.aii.crm.agr.persistence.req.AgrReqDto;
+import java.util.List;
 
 public interface IAgreementSV {
 	Long insertAgreement(AgrReqDto agrReqDto);
+
+	List<AgrAgreement> queryAgreementByCustomerIdAndOrderId(Long customerId, Long customerOrderId, Long agrStateValid);
+
+	Integer updateAgreementStatusBatch(List<AgrAgreement> agreementList, Long agreementStatus);
 }
