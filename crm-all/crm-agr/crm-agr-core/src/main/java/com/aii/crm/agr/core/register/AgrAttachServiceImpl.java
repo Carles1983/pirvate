046008b81914251865b16af01b18804a552f8804
Carles1983 <soane1983@163.com>
2019-05-18 11:17:14 +0800

    Update AgrAttachServiceImpl.java

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/register/AgrAttachServiceImpl.java
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/register/AgrAttachServiceImpl.java
@@ -3,7 +3,7 @@ package com.aii.crm.agr.core.register;
 import com.aii.crm.agr.api.AgrAttachService;
 import com.aii.crm.agr.core.persistence.bo.AgrAttach;
 import com.aii.crm.agr.core.service.interfaces.IAgrAttachSV;
-import com.aii.crm.agr.persistence.AgrAttachDto;
+import com.aii.crm.agr.persistence.req.AgrAttachReqDto;
 import com.alibaba.dubbo.config.annotation.Service;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +15,9 @@ public class AgrAttachServiceImpl implements AgrAttachService {
 	private IAgrAttachSV agrAttachSV;
 
 	@Override
-	public Integer saveAgrAttach(AgrAttachDto agrAttachDto) throws Exception {
+	public Integer saveAgrAttach(AgrAttachReqDto agrAttachReqDto) throws Exception {
 		AgrAttach attach = new AgrAttach();
-		BeanUtils.copyProperties(agrAttachDto, attach);
+		BeanUtils.copyProperties(agrAttachReqDto, attach);
 		return agrAttachSV.insertAgrAttach(attach);
 	}
 }
