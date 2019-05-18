b8260e66e3baeaf56dac18e21feac8715dc003ca
Carles1983 <soane1983@163.com>
2019-05-18 13:04:57 +0800

    Update AgrConstant.java

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/constant/AgrConstant.java
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/constant/AgrConstant.java
@@ -7,4 +7,18 @@ public class AgrConstant {
 	public static final Long AGR_STATE_INVALID = 0L;
 	public static final Long AGR_STATE_VALID = 1L;
 
+	//协议状态
+	public static final long AGR_STATUS_DRAFT = 9010L;
+	public static final long AGR_STATUS_NEGOTIATED = 9020L;
+	public static final long AGR_STATUS_CONFIRM = 9021L;
+	public static final long AGR_STATUS_PRESIGN = 9025L;
+	public static final long AGR_STATUS_SIGN = 9030L;
+	public static final long AGR_STATUS_VALIDATED = 9050L;
+	public static final long AGR_STATUS_EXPIRE = 9060L;
+	public static final long AGR_STATUS_TERMI = 9070L;
+	public static final long AGR_STATUS_NEGOTIATING = 9080L;
+	public static final long AGR_STATUS_PROLONGING = 9090L;
+	public static final long AGR_STATUS_PROLONGED = 9100L;
+	public static final long AGR_STATUS_LOST = 9110L;
+
 }
