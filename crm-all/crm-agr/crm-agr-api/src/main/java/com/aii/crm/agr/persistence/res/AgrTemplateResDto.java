6b54f1be2ab0f4d8cf41894767d533b3d29aade7
Carles1983 <soane1983@163.com>
2019-05-18 11:17:11 +0800

    Create AgrTemplateResDto.java

---
+++ crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/crm/agr/persistence/res/AgrTemplateResDto.java
@@ -0,0 +1,26 @@
+package com.aii.crm.agr.persistence.res;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class AgrTemplateResDto implements Serializable {
+	private Long agreementTemplateId;
+	private Long agrTypeId;
+	private String templateCode;
+	private String templateStatus;
+	private Date statusTime;
+	private Long upTemplateId;
+	private String agreementTemplateName;
+	private Long agrLevelId;
+	private String agrDocUrl;
+	private Date createDate;
+	private Long createOpId;
+	private Long createOrgId;
+	private Date doneDate;
+	private Long state;
+	private Long isAddendum;
+	private Long offerId;
+	private String language;
+}
