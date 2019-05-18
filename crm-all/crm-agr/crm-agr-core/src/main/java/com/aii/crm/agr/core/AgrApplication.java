0f806cbf80ac92ff5529c79871348f3d423bcd03
Carles1983 <soane1983@163.com>
2019-05-18 14:18:27 +0800

    Update AgrApplication.java

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/AgrApplication.java
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/AgrApplication.java
@@ -1,10 +1,16 @@
 package com.aii.crm.agr.core;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDubbo
+@EnableDiscoveryClient
+@MapperScan(basePackages = { "com.aii.crm.agr.core.persistence" })
 public class AgrApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder().web(WebApplicationType.NONE).run(args);
