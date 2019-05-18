3a83c59acc4286fe220aa735763bebc94b8e108f
Carles1983 <soane1983@163.com>
2019-05-18 12:51:25 +0800

    Create AgrConstant.java

---
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/constant/AgrConstant.java
@@ -0,0 +1,10 @@
+package com.aii.crm.agr.core.constant;
+
+public class AgrConstant {
+
+	private AgrConstant(){}
+
+	public static final Long AGR_STATE_INVALID = 0L;
+	public static final Long AGR_STATE_VALID = 1L;
+
+}
