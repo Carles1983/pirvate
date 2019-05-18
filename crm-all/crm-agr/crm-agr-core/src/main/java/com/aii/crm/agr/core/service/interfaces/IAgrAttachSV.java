05de652b8b117cbdbed37bf86afd722cb1ec7fc3
Carles1983 <soane1983@163.com>
2019-05-18 11:02:28 +0800

    Update IAgrAttachSV.java

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/interfaces/IAgrAttachSV.java
+++ crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/interfaces/IAgrAttachSV.java
@@ -1,4 +1,7 @@
 package com.aii.crm.agr.core.service.interfaces;
 
+import com.aii.crm.agr.core.persistence.bo.AgrAttach;
+
 public interface IAgrAttachSV {
+	Integer insertAgrAttach(AgrAttach attach);
 }
