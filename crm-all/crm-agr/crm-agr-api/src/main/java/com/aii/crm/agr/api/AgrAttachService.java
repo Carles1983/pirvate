d424209774e16e49884d05bae2e8ba047c166cf0
Carles1983 <soane1983@163.com>
2019-05-18 11:03:29 +0800

    Update AgrAttachService.java

--- crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/crm/agr/api/AgrAttachService.java
+++ crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/crm/agr/api/AgrAttachService.java
@@ -3,5 +3,5 @@ package com.aii.crm.agr.api;
 import com.aii.crm.agr.persistence.AgrAttachDto;
 
 public interface AgrAttachService {
-	Long saveAgrAttach(AgrAttachDto agrAttachDto) throws Exception;
+	Integer saveAgrAttach(AgrAttachDto agrAttachDto) throws Exception;
 }
