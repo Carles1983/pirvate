4415e079ba63cc595a210daa064ad1a8fc6d6299
Carles1983 <soane1983@163.com>
2019-05-18 11:17:00 +0800

    Create AgrReqDto.java

---
+++ crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/crm/agr/persistence/req/AgrReqDto.java
@@ -0,0 +1,45 @@
+package com.aii.crm.agr.persistence.req;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class AgrReqDto implements Serializable {
+	private Long agreementId;
+	private Long agreementTemplateId;
+	private String agreementTempCode;
+	private String agrBarCode;
+	private Long agrTypeId;
+	private String agreementName;
+	private String agreementCode;
+	private Long ownerId;
+	private String autoRenew;
+	private String renewCircle;
+	private Long agrLevelId;
+	private String secrecyLevel;
+	private Date signTime;
+	private String signPlace;
+	private String agrDocUrl;
+	private String description;
+	private String agrSource;
+	private Long agrSourceObjId;
+	private String agrSourceBizId;
+	private Date effDate;
+	private Date expDate;
+	private Long agreementStatus;
+	private Date statusTime;
+	private Date parentCreateDate;
+	private String emailStatus;
+	private String breachStatus;
+	private Long isAddendum;
+	private Long renewTimes;
+	private Long state;
+	private String ownerType;
+	private String agrSourceSubObjId;
+	private Long agrTempVersionId;
+	private String billCycle;
+	private String paymentTerms;
+	private String billNo;
+	private String cvrNo;
+}
