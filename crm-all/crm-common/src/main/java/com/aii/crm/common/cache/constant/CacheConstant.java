76be3b07c5adef53ee677b7f6b94cb598fc94209
Carles1983 <soane1983@163.com>
2019-05-20 11:58:20 +0800

    Create CacheConstant.java

---
+++ crm-all/crm-common/src/main/java/com/aii/crm/common/cache/constant/CacheConstant.java
@@ -0,0 +1,9 @@
+package com.aii.crm.common.cache.constant;
+
+public class CacheConstant {
+
+	private CacheConstant(){}
+
+	public static final String DISTRICT_REDIS_KEY = "cache_bs_district";
+
+}
