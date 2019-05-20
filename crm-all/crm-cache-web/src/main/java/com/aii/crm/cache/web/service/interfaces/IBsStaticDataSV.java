1fac41c639cb83a03204c452055ea32004a87b5e
Carles1983 <soane1983@163.com>
2019-05-20 17:46:30 +0800

    Update IBsStaticDataSV.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsStaticDataSV.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsStaticDataSV.java
@@ -1,4 +1,12 @@
 package com.aii.crm.cache.web.service.interfaces;
 
+import com.aii.crm.common.cache.model.StaticData;
+import com.aii.crm.common.page.PageResult;
+
 public interface IBsStaticDataSV {
+	Integer saveBsStaticData(StaticData addDto);
+	PageResult<StaticData> listBsStaticDataPages(Integer pageNo, Integer pageSize);
+	Integer updateBsStaticData(StaticData updateDto);
+	Integer deleteBsStaticData(String codeType, String codeValue);
+	Integer loadBsStaticData();
 }
