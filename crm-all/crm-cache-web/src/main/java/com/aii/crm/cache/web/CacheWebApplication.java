a0de9c33fcb9308f164f0f1313c866e0d9b27ac7
Carles1983 <soane1983@163.com>
2019-05-20 10:23:17 +0800

    Update CacheWebApplication.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/CacheWebApplication.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/CacheWebApplication.java
@@ -1,4 +1,17 @@
 package com.aii.crm.cache.web;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.aii.crm.cache.web.persistence")
+@ComponentScan(basePackages = {"com.aii.component"})
 public class CacheWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CacheWebApplication.class, args);
+	}
 }
