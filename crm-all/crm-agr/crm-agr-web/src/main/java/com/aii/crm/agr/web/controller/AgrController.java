1b7cc1034d853425eeb266a8d28bc644bee76978
Carles1983 <soane1983@163.com>
2019-05-20 09:31:35 +0800

    Update AgrController.java

--- crm-all/crm-agr/crm-agr-web/src/main/java/com/aii/crm/agr/web/controller/AgrController.java
+++ crm-all/crm-agr/crm-agr-web/src/main/java/com/aii/crm/agr/web/controller/AgrController.java
@@ -1,9 +1,39 @@
 package com.aii.crm.agr.web.controller;
 
+import com.aii.crm.agr.persistence.req.AgrDataReqDto;
+import com.aii.crm.agr.persistence.res.AgrDataResDto;
+import com.aii.crm.common.exception.CrmCheckedException;
+import com.aii.crm.common.exception.CrmCheckedException;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/agr")
+@Slf4j
 public class AgrController {
+
+	@RequestMapping(value = "/preview", method = RequestMethod.POST)
+	public List<AgrDataResDto> previewAgreement(@RequestBody AgrDataReqDto agrDataReqDto) throws CrmCheckedException {
+		if(paramVerify(agrDataReqDto)){
+			throw new CrmCheckedException("illegalArguments", new Exception("eee"));
+		}
+
+
+
+		return null;
+	}
+
+
+	private boolean paramVerify(AgrDataReqDto agrDataReqDto){
+		if(agrDataReqDto == null || agrDataReqDto.getOrderCustomerId() == null || agrDataReqDto.getCustId() == null || agrDataReqDto.getCustType() == null){
+			return true;
+		}
+		return false;
+	}
+
+
 }
