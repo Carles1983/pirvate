6810e174faf2b7e7e6494107e143a16334b322d6
Carles1983 <soane1983@163.com>
2019-05-20 17:36:39 +0800

    Update BsParaDetailSVImpl.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/impl/BsParaDetailSVImpl.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/impl/BsParaDetailSVImpl.java
@@ -1,23 +1,28 @@
 package com.aii.crm.cache.web.service.impl;
 
 import com.aii.crm.cache.web.constant.CacheWebConstant;
-import com.aii.crm.cache.web.persistence.bo.BsFtp;
-import com.aii.crm.cache.web.persistence.bo.BsFtpExample;
 import com.aii.crm.cache.web.persistence.bo.BsParaDetail;
 import com.aii.crm.cache.web.persistence.bo.BsParaDetailExample;
+import com.aii.crm.cache.web.persistence.bo.BsParaDetailKey;
 import com.aii.crm.cache.web.persistence.mapper.BsParaDetailMapper;
 import com.aii.crm.cache.web.service.interfaces.IBsParaDetailSV;
 import com.aii.crm.common.bean.BeanConvertUtil;
-import com.aii.crm.common.cache.model.Ftp;
+import com.aii.crm.common.cache.constant.CacheConstant;
 import com.aii.crm.common.cache.model.ParaDetail;
+import com.aii.crm.common.map.MapUtil;
 import com.aii.crm.common.page.PageResult;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 @Service
 @Slf4j
@@ -60,20 +65,67 @@ public class BsParaDetailSVImpl implements IBsParaDetailSV {
 
 	@Override
 	public Integer updateBsParaDetail(ParaDetail updateDto) {
-		return null;
+		try {
+			BsParaDetail bsParaDetail = BeanConvertUtil.beanConversion(updateDto, BsParaDetail.class);
+			Integer result = paraDetailMapper.updateByPrimaryKeySelective(bsParaDetail);
+			modifyRedisParaDetail(updateDto, updateDto.getRegionId(), updateDto.getParaType(), updateDto.getParaCode(),
+					CacheWebConstant.MODIFY_TYPE_UPDATE);
+			return result;
+		} catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+			log.info("error");
+		}
+		return 0;
 	}
 
 	@Override
-	public Integer deleteBsParaDetail(String ftpCode) {
-		return null;
+	public Integer deleteBsParaDetail(String regionId, String paraType, String paraCode) {
+		BsParaDetailKey key = new BsParaDetailKey();
+		key.setRegionId(regionId);
+		key.setParaType(paraType);
+		key.setParaCode(paraCode);
+		BsParaDetail bsParaDetail = paraDetailMapper.selectByPrimaryKey(key);
+		bsParaDetail.setState(CacheWebConstant.STATE_OUT_OF_USE);
+		Integer result = paraDetailMapper.updateByPrimaryKeySelective(bsParaDetail);
+		modifyRedisParaDetail(null, regionId, paraType, paraCode, CacheWebConstant.MODIFY_TYPE_DELETE);
+		return result;
 	}
 
 	@Override
 	public Integer loadBsParaDetail() {
-		return null;
+		BsParaDetailExample example = new BsParaDetailExample();
+		BsParaDetailExample.Criteria criteria = example.createCriteria();
+		criteria.andStateEqualTo(CacheWebConstant.STATE_IN_USE);
+		List<BsParaDetail> bsParaDetailList = paraDetailMapper.selectByExample(example);
+		try {
+			List<ParaDetail> paraDetailList = BeanConvertUtil.listConversion(bsParaDetailList, ParaDetail.class);
+			if (!CollectionUtils.isEmpty(paraDetailList)) {
+				Map<String, Object> map = new HashMap<>();
+				for (ParaDetail paraDetail : paraDetailList) {
+					map.put(paraDetail.getRegionId() + "_" + paraDetail.getParaType() + "_" + paraDetail.getParaCode(), paraDetail);
+				}
+				HashOperations<String, String, Object> hashOp = redisTemplate.opsForHash();
+				Map<String, Object> redisMap = hashOp.entries(CacheConstant.PARA_DETAIL_REDIS_KEY);
+				Map<String, Object> differMap = MapUtil.getDifferenceSet(redisMap, map);
+				hashOp.putAll(CacheConstant.PARA_DETAIL_REDIS_KEY, map);
+				hashOp.delete(CacheConstant.PARA_DETAIL_REDIS_KEY, differMap.keySet().toArray());
+			}
+			return paraDetailList.size();
+		} catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+			log.info("error");
+		}
+		return 0;
 	}
 
-	private void modifyRedisParaDetail(ParaDetail addDto, String regionId, String paraType, String paraCode, int modifyTypeAdd) {
+	private void modifyRedisParaDetail(ParaDetail addDto, String regionId, String paraType, String paraCode, int modifyType) {
+		HashOperations<String, String, Object> hashOp = redisTemplate.opsForHash();
+		if (CacheWebConstant.MODIFY_TYPE_ADD == modifyType || CacheWebConstant.MODIFY_TYPE_UPDATE == modifyType) {
+			hashOp.put(CacheConstant.FTP_REDIS_KEY,
+					addDto.getRegionId()+"_"+addDto.getParaType()+"_"+addDto.getParaCode(), addDto);
+		} else if (CacheWebConstant.MODIFY_TYPE_DELETE == modifyType) {
+			hashOp.delete(CacheConstant.FTP_REDIS_KEY, regionId+"_"+paraType+"_"+paraCode);
+		} else {
+			log.info("not implemented.");
+		}
 	}
 
 }
