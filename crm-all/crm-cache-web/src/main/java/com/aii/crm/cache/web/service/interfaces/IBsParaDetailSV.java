64f48669f11030d49e329ec34178f5fe9a2363c7
Carles1983 <soane1983@163.com>
2019-05-20 17:08:56 +0800

    Update IBsParaDetailSV.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsParaDetailSV.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsParaDetailSV.java
@@ -1,4 +1,12 @@
 package com.aii.crm.cache.web.service.interfaces;
 
+import com.aii.crm.common.cache.model.ParaDetail;
+import com.aii.crm.common.page.PageResult;
+
 public interface IBsParaDetailSV {
+	Integer saveBsParaDetail(ParaDetail addDto);
+	PageResult<ParaDetail> listBsParaDetailPages(Integer pageNo, Integer pageSize);
+	Integer updateBsParaDetail(ParaDetail updateDto);
+	Integer deleteBsParaDetail(String ftpCode);
+	Integer loadBsParaDetail();
 }
