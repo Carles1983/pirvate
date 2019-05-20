3bfc0c40eedf6a196f4d3fc719035f4ff706cbc2
Carles1983 <soane1983@163.com>
2019-05-20 17:26:43 +0800

    Update BsParaDetailSVImpl.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/impl/BsParaDetailSVImpl.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/impl/BsParaDetailSVImpl.java
@@ -1,14 +1,21 @@
 package com.aii.crm.cache.web.service.impl;
 
+import com.aii.crm.cache.web.constant.CacheWebConstant;
+import com.aii.crm.cache.web.persistence.bo.BsFtp;
+import com.aii.crm.cache.web.persistence.bo.BsParaDetail;
 import com.aii.crm.cache.web.persistence.mapper.BsParaDetailMapper;
 import com.aii.crm.cache.web.service.interfaces.IBsParaDetailSV;
+import com.aii.crm.common.bean.BeanConvertUtil;
 import com.aii.crm.common.cache.model.ParaDetail;
 import com.aii.crm.common.page.PageResult;
+import java.lang.reflect.InvocationTargetException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class BsParaDetailSVImpl implements IBsParaDetailSV {
 
 	@Autowired
@@ -19,7 +26,17 @@ public class BsParaDetailSVImpl implements IBsParaDetailSV {
 
 	@Override
 	public Integer saveBsParaDetail(ParaDetail addDto) {
-		return null;
+		try {
+			addDto.setState(CacheWebConstant.STATE_IN_USE);
+			BsParaDetail bsParaDetail = BeanConvertUtil.beanConversion(addDto, BsParaDetail.class);
+			Integer result = paraDetailMapper.insertSelective(bsParaDetail);
+			modifyRedisParaDetail(addDto, addDto.getRegionId(), addDto.getParaType(), addDto.getParaCode(),
+					CacheWebConstant.MODIFY_TYPE_ADD);
+			return result;
+		} catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+			log.info("error");
+		}
+		return 0;
 	}
 
 	@Override
@@ -41,4 +58,8 @@ public class BsParaDetailSVImpl implements IBsParaDetailSV {
 	public Integer loadBsParaDetail() {
 		return null;
 	}
+
+	private void modifyRedisParaDetail(ParaDetail addDto, String regionId, String paraType, String paraCode, int modifyTypeAdd) {
+	}
+
 }
