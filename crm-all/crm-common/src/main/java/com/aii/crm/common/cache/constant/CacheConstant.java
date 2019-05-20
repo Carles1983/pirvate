da724ce5a6622785a8d0b009aeb91592baf1ab79
Carles1983 <soane1983@163.com>
2019-05-20 17:36:41 +0800

    Update CacheConstant.java

--- crm-all/crm-common/src/main/java/com/aii/crm/common/cache/constant/CacheConstant.java
+++ crm-all/crm-common/src/main/java/com/aii/crm/common/cache/constant/CacheConstant.java
@@ -7,6 +7,7 @@ public class CacheConstant {
 	public static final String DISTRICT_REDIS_KEY = "cache_bs_district";
 	public static final String FTP_REDIS_KEY = "cache_ftp";
 	public static final String FTP_PATH_REDIS_KEY = "cache_ftp_path";
-	public static final String I18N_RESOURCE_REDIS_KEY = "cache_single_i18n_resource";
+	public static final String I18N_RESOURCE_REDIS_KEY = "cache_i18n_resource";
+	public static final String PARA_DETAIL_REDIS_KEY = "cache_para_detail";
 
 }
