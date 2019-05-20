41a3270a4dc15f5ff171e74bf8df23eeb3bcc3ac
Carles1983 <soane1983@163.com>
2019-05-20 14:48:50 +0800

    Update CacheController.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/controller/CacheController.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/controller/CacheController.java
@@ -1,6 +1,7 @@
 package com.aii.crm.cache.web.controller;
 
 import com.aii.crm.cache.web.service.interfaces.IBsDistrictSV;
+import com.aii.crm.cache.web.service.interfaces.IBsFtpSV;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,9 +14,17 @@ public class CacheController {
 	@Autowired
 	private IBsDistrictSV districtSV;
 
+	@Autowired
+	private IBsFtpSV ftpSV;
+
 	@RequestMapping(value = "/district/redis", method = RequestMethod.PUT)
 	public Integer loadBsDistrictToRedis(){
 		return districtSV.loadDistrict();
+	}
+
+	@RequestMapping(value = "/ftp/redis", method = RequestMethod.PUT)
+	public Integer loadBsFtpToRedis(){
+		return ftpSV.loadFtp();
 	}
 
 }
