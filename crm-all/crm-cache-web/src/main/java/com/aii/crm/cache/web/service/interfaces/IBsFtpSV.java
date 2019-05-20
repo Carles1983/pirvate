9d340f92fd2c07348f51af0b378473898b23f083
Carles1983 <soane1983@163.com>
2019-05-20 14:44:40 +0800

    Update IBsFtpSV.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsFtpSV.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsFtpSV.java
@@ -1,4 +1,13 @@
 package com.aii.crm.cache.web.service.interfaces;
 
+import com.aii.crm.common.cache.model.Ftp;
+import com.aii.crm.common.page.PageResult;
+
+
 public interface IBsFtpSV {
+	Integer saveFtp(Ftp addDto);
+	PageResult<Ftp> listFtpPages(Integer pageNo, Integer pageSize);
+	Integer updateFtp(Ftp updateDto);
+	Integer deleteFtp(String ftpCode);
+	Integer loadFtp();
 }
