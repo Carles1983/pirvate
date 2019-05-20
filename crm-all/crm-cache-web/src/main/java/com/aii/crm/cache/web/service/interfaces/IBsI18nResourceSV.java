adca82f9ad9aba2ce9f59d9ca352a5be4de7254e
Carles1983 <soane1983@163.com>
2019-05-20 16:07:36 +0800

    Update IBsI18nResourceSV.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsI18nResourceSV.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsI18nResourceSV.java
@@ -1,4 +1,12 @@
 package com.aii.crm.cache.web.service.interfaces;
 
+import com.aii.crm.common.cache.model.I18nResource;
+import com.aii.crm.common.page.PageResult;
+
 public interface IBsI18nResourceSV {
+	Integer saveI18nResource(I18nResource addDto);
+	PageResult<I18nResource> listI18nPages(Integer pageNo, Integer pageSize);
+	Integer updateI18nResource(I18nResource updateDto);
+	Integer deleteI18nResource(String resKey, String language);
+	Integer loadI18nResource();
 }
