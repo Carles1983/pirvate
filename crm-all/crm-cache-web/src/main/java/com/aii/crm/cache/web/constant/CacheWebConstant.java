9865e8686161e3df772f14d1471cd7dd59f16eaf
Carles1983 <soane1983@163.com>
2019-05-20 14:44:23 +0800

    Create CacheWebConstant.java

---
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/constant/CacheWebConstant.java
@@ -0,0 +1,14 @@
+package com.aii.crm.cache.web.constant;
+
+public class CacheWebConstant {
+
+	private CacheWebConstant(){}
+
+	// 新增
+	public static final int MODIFY_TYPE_ADD = 0;
+	// 修改
+	public static final int MODIFY_TYPE_UPDATE = 1;
+	// 删除
+	public static final int MODIFY_TYPE_DELETE = 2;
+
+}
