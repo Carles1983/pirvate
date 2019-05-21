33b9396afe07abbe10808f1f822365d8a97db3e6
Carles1983 <soane1983@163.com>
2019-05-21 15:06:29 +0800

    update web

--- crm-all/crm-agr/crm-agr-web/src/main/java/com/aii/crm/agr/web/AgrWebApplication.java
+++ crm-all/crm-agr/crm-agr-web/src/main/java/com/aii/crm/agr/web/AgrWebApplication.java
@@ -1,4 +1,17 @@
 package com.aii.crm.agr.web;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.aii.crm.agr.web.persistence")
+@ComponentScan(basePackages = {"com.aii.component"})
 public class AgrWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AgrWebApplication.class, args);
+	}
 }
