22a0cbd0b30cfe742b6917b6c4e19d438bb1a67f
Carles1983 <soane1983@163.com>
2019-05-21 09:10:49 +0800

    Create BsTenantController.java

---
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/controller/BsTenantController.java
@@ -0,0 +1,39 @@
+package com.aii.crm.cache.web.controller;
+
+import com.aii.crm.cache.web.service.interfaces.IBsTenantSV;
+import com.aii.crm.common.cache.model.Tenant;
+import com.aii.crm.common.page.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cache")
+public class BsTenantController {
+
+	@Autowired
+	private IBsTenantSV tenantSV;
+
+	@RequestMapping(value = "/paraDetail", method = RequestMethod.POST)
+	public Integer createBsTenant(@RequestBody Tenant addDto){
+		return tenantSV.saveTenant(addDto);
+	}
+
+	@RequestMapping(value = "/ftpPaths", method = RequestMethod.GET)
+	public PageResult<Tenant> retrieveBsTenant(@RequestParam Integer pageNo, @RequestParam Integer pageSize){
+		return tenantSV.listTenantPages(pageNo, pageSize);
+	}
+
+	@RequestMapping(value = "/ftpPath", method = RequestMethod.PUT)
+	public Integer updateBsTenant(@RequestBody Tenant updateDto){
+		return tenantSV.updateTenant(updateDto);
+	}
+
+	@RequestMapping(value = "/ftpPath", method = RequestMethod.DELETE)
+	public Integer deleteBsTenant(@RequestParam String tenantId){
+		return tenantSV.deleteTenant(tenantId);
+	}
+}
