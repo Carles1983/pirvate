e51493be6da04afc70d9383bfa3ec893e6112238
Carles1983 <soane1983@163.com>
2019-05-21 09:41:28 +0800

    Create ICfgClientTimeoutSV.java

---
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/ICfgClientTimeoutSV.java
@@ -0,0 +1,12 @@
+package com.aii.crm.cache.web.service.interfaces;
+
+import com.aii.crm.common.cache.model.ClientTimeout;
+import com.aii.crm.common.page.PageResult;
+
+public interface ICfgClientTimeoutSV {
+	Integer saveClientTimeout(ClientTimeout addDto);
+	PageResult<ClientTimeout> listClientTimeoutPages(Integer pageNo, Integer pageSize);
+	Integer updateClientTimeout(ClientTimeout updateDto);
+	Integer deleteClientTimeout(String interfaceClassName, String methodName, Integer parameterCount);
+	Integer loadClientTimeout();
+}
