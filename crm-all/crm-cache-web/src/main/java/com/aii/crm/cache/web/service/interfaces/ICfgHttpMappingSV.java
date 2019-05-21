f17538853685df20b9b2420a6d6e5c46295cd2b1
Carles1983 <soane1983@163.com>
2019-05-21 10:30:59 +0800

    Update ICfgHttpMappingSV.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/ICfgHttpMappingSV.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/ICfgHttpMappingSV.java
@@ -1,4 +1,12 @@
 package com.aii.crm.cache.web.service.interfaces;
 
+import com.aii.crm.common.cache.model.HttpMapping;
+import com.aii.crm.common.page.PageResult;
+
 public interface ICfgHttpMappingSV {
+	Integer saveHttpMapping(HttpMapping addDto);
+	PageResult<HttpMapping> listHttpMappingPages(Integer pageNo, Integer pageSize);
+	Integer updateHttpMapping(HttpMapping updateDto);
+	Integer deleteHttpMapping(Long mappingId);
+	Integer loadHttpMapping();
 }
