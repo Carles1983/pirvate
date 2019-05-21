eb207bb1a55288baa4d54aa26f7c51236bc85864
Carles1983 <soane1983@163.com>
2019-05-21 15:30:47 +0800

    create ci database

---
+++ db/ci/ci_channel_mapping.sql
@@ -0,0 +1,24 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.6.36-82.0-log : Database - ci
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ci` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ci`;
+
+/*Data for the table `ci_channel_mapping` */
+
+insert  into `ci_channel_mapping`(`CHNL_MAPPING_ID`,`CHANNEL_ID`,`SRC_SYS_ID`,`SRC_SYS_CHNL_ID`,`STATE`,`OP_ID`,`ORG_ID`,`DONE_DATE`,`REMARK`) values (1,100,'CRM','1',1,NULL,NULL,'2017-06-28 15:36:36','ESHOP'),(2,101,'CRM','2',1,NULL,NULL,'2017-06-28 15:36:36','AgentView'),(3,102,'CBS','3',1,NULL,NULL,'2017-06-28 15:36:36','CBS'),(5,105,'CRM','5',1,NULL,NULL,'2018-12-27 11:21:04','Chatbot'),(6,106,'CRM','6',1,NULL,NULL,'2018-12-27 11:21:04','RTSS'),(7,107,'CRM','7',1,NULL,NULL,'2018-12-27 11:21:04','SCC'),(8,108,'CRM','8',1,NULL,NULL,'2018-12-27 11:21:04','Wechat'),(9,109,'CRM','9',1,NULL,NULL,'2018-12-27 11:21:04','Facebook');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ db/ci/ci_interaction_category.sql
@@ -0,0 +1,24 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.6.36-82.0-log : Database - ci
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ci` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ci`;
+
+/*Data for the table `ci_interaction_category` */
+
+insert  into `ci_interaction_category`(`INTERACTION_CATEGORY_ID`,`PARENT_CATEGORY_ID`,`INTERACTION_CATEGORY_NAME`,`INTERACTION_CATEGORY_NAME_I18N`,`OP_ID`,`ORG_ID`,`DONE_DATE`,`REMAKR`) values (1,0,'Sales','',0,0,'2017-06-28 15:38:58',NULL),(2,0,'Marketing Campaign','',0,0,'2017-06-28 15:38:59',NULL),(3,0,'Trouble Ticket','',0,0,'2017-06-28 15:38:59',NULL),(4,0,'Knowledge Base','',0,0,'2017-06-28 15:39:00',NULL),(5,0,'Order Management','',0,0,'2017-06-28 15:39:00',NULL),(6,0,'Push Communication','',0,0,'2017-06-28 15:39:01',NULL),(7,0,'Call Center','',0,0,'2017-06-28 15:39:01',NULL),(8,0,'Billing','',0,0,'2017-06-28 15:39:02',NULL),(9,0,'Customer Management','',0,0,'2017-06-28 15:39:02',NULL),(10,0,'EShop','',0,0,'2017-06-28 15:39:02',NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
