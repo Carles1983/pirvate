42394486f458c1d7e3d445ad422268e388c9d2c7
Carles1983 <soane1983@163.com>
2019-05-21 10:01:07 +0800

    Update ICfgHttpSV.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/ICfgHttpSV.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/ICfgHttpSV.java
@@ -1,4 +1,12 @@
 package com.aii.crm.cache.web.service.interfaces;
 
+import com.aii.crm.common.cache.model.Http;
+import com.aii.crm.common.page.PageResult;
+
 public interface ICfgHttpSV {
+	Integer saveHttp(Http addDto);
+	PageResult<Http> listHttpPages(Integer pageNo, Integer pageSize);
+	Integer updateHttp(Http updateDto);
+	Integer deleteHttp(String ftpCode);
+	Integer loadHttp();
 }
