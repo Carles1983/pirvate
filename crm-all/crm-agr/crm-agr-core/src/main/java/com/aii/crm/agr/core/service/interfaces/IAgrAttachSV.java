9cd6c787c495aa2398698a6ea158952113cd4f82
Carles1983 <soane1983@163.com>
2019-05-21 14:42:24 +0800

    delete core

--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/interfaces/IAgrAttachSV.java
+++
@@ -1,7 +0,0 @@
-package com.aii.crm.agr.core.service.interfaces;
-
-import com.aii.crm.agr.core.persistence.bo.AgrAttach;
-
-public interface IAgrAttachSV {
-	Integer insertAgrAttach(AgrAttach attach);
-}
--- crm-all/crm-agr/crm-agr-core/src/main/java/com/aii/crm/agr/core/service/interfaces/IAgrTemplateSV.java
+++
@@ -1,8 +0,0 @@
-package com.aii.crm.agr.core.service.interfaces;
-
-import com.aii.crm.agr.core.persistence.bo.AgrTemplate;
-import java.util.List;
-
-public interface IAgrTemplateSV {
-	List<AgrTemplate> listAgrTempalteByOfferId(Long offerId);
-}
