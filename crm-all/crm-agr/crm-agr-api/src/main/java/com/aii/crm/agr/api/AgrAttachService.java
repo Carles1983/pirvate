92c0155480b27ad169138ad3a5c50e347d5759bd
Carles1983 <soane1983@163.com>
2019-05-21 14:41:26 +0800

    delete core

--- crm-all/crm-agr/crm-agr-api/src/main/java/com/aii/crm/agr/api/AgrAttachService.java
+++
@@ -1,8 +0,0 @@
-package com.aii.crm.agr.api;
-
-
-import com.aii.crm.agr.persistence.req.AgrAttachReqDto;
-
-public interface AgrAttachService {
-	Integer saveAgrAttach(AgrAttachReqDto agrAttachReqDto) throws Exception;
-}
