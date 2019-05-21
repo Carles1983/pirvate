8e4943a0bbac6efc9801071a616cec25469c22b2
Carles1983 <soane1983@163.com>
2019-05-21 08:50:33 +0800

    Update IBsDistrictSV.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsDistrictSV.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsDistrictSV.java
@@ -2,16 +2,11 @@ package com.aii.crm.cache.web.service.interfaces;
 
 import com.aii.crm.common.cache.model.District;
 import com.aii.crm.common.page.PageResult;
-import com.github.pagehelper.Page;
 
 public interface IBsDistrictSV {
 	Integer saveDistrict(District addDto);
-
 	PageResult<District> listDistrictPages(Integer pageNo, Integer pageSize);
-
 	Integer updateDistrict(District updateDto);
-
 	Integer deleteDistrict(Long districtId);
-
 	Integer loadDistrict();
 }
