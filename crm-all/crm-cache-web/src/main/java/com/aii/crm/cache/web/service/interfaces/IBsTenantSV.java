8e79c4849bfbfd973409d08c1e94e665517c8e4c
Carles1983 <soane1983@163.com>
2019-05-21 08:50:39 +0800

    Update IBsTenantSV.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsTenantSV.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IBsTenantSV.java
@@ -1,4 +1,12 @@
 package com.aii.crm.cache.web.service.interfaces;
 
+import com.aii.crm.common.cache.model.Tenant;
+import com.aii.crm.common.page.PageResult;
+
 public interface IBsTenantSV {
+	Integer saveTenant(Tenant addDto);
+	PageResult<Tenant> listTenantPages(Integer pageNo, Integer pageSize);
+	Integer updateTenant(Tenant updateDto);
+	Integer deleteTenant(String ftpCode);
+	Integer loadTenant();
 }
