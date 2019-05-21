ae8d552ecfa2e4215ff6463b957b27c85bcf6081
Carles1983 <soane1983@163.com>
2019-05-21 10:59:00 +0800

    Update IExceptionCodeSV.java

--- crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IExceptionCodeSV.java
+++ crm-all/crm-cache-web/src/main/java/com/aii/crm/cache/web/service/interfaces/IExceptionCodeSV.java
@@ -1,4 +1,12 @@
 package com.aii.crm.cache.web.service.interfaces;
 
+import com.aii.crm.common.cache.model.ExCode;
+import com.aii.crm.common.page.PageResult;
+
 public interface IExceptionCodeSV {
+	Integer saveExceptionCode(ExCode addDto);
+	PageResult<ExCode> listExceptionCodePages(Integer pageNo, Integer pageSize);
+	Integer updateExceptionCode(ExCode updateDto);
+	Integer deleteExceptionCode(String exceptionCodeType);
+	Integer loadExceptionCode();
 }
