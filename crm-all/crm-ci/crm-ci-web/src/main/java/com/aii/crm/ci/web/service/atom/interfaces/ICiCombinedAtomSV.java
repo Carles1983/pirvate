53808a8a043219ba104302ddfd04f89723abd5ae
Carles1983 <soane1983@163.com>
2019-05-22 16:39:14 +0800

    Update ICiCombinedAtomSV.java

--- crm-all/crm-ci/crm-ci-web/src/main/java/com/aii/crm/ci/web/service/atom/interfaces/ICiCombinedAtomSV.java
+++ crm-all/crm-ci/crm-ci-web/src/main/java/com/aii/crm/ci/web/service/atom/interfaces/ICiCombinedAtomSV.java
@@ -2,9 +2,10 @@ package com.aii.crm.ci.web.service.atom.interfaces;
 
 import com.aii.crm.ci.web.dto.req.CiInteractionReqDto;
 import com.aii.crm.ci.web.persistence.bo.CiContact;
+import java.text.ParseException;
 
 public interface ICiCombinedAtomSV {
-	CiInteractionReqDto dealInteractionAndAttrValue(CiInteractionReqDto interactionReqDto);
+	CiInteractionReqDto dealInteractionAndAttrValue(CiInteractionReqDto interactionReqDto) throws ParseException;
 
 	boolean saveContact(CiContact contact, CiInteractionReqDto interactionReqDto);
 }
