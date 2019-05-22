d3cc93258e29c16224e7299e89095eda4c34fe1e
Carles1983 <soane1983@163.com>
2019-05-22 18:18:25 +0800

    Update CiCacheOperation.java

--- crm-all/crm-ci/crm-ci-web/src/main/java/com/aii/crm/ci/web/cache/CiCacheOperation.java
+++ crm-all/crm-ci/crm-ci-web/src/main/java/com/aii/crm/ci/web/cache/CiCacheOperation.java
@@ -1,5 +1,6 @@
 package com.aii.crm.ci.web.cache;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -21,14 +22,39 @@ public class CiCacheOperation {
 		return hashOp.get(key, hKey);
 	}
 
-	public Long deleteCiComponentFromCache(String key, String hKey){
+	public <T> Map<String, T> getCiStringComponentFromCache(String key){
+		HashOperations<String, String, T> hashOp = redisTemplate.opsForHash();
+		return hashOp.entries(key);
+	}
+
+	public <T> Map<Long, T> getCiLongComponentFromCache(String key){
+		HashOperations<String, Long, T> hashOp = redisTemplate.opsForHash();
+		return hashOp.entries(key);
+	}
+
+	public Long deleteCiComponentFromCache(String key, Object hKey){
 		HashOperations<String, Long, Object> hashOp = redisTemplate.opsForHash();
 		return hashOp.delete(key, hKey);
+	}
+
+	public Long deleteCiComponentFromCache(String key, Object[] hKeys){
+		HashOperations<String, Long, Object> hashOp = redisTemplate.opsForHash();
+		return hashOp.delete(key, hKeys);
 	}
 
 	public <T> void putCiComponentFromCache(String key, String hKey, T t){
 		HashOperations<String, String, T> hashOp = redisTemplate.opsForHash();
 		hashOp.put(key, hKey, t);
+	}
+
+	public <T> void putCiStringComponentFromCache(String key, Map<String, T> map){
+		HashOperations<String, String, T> hashOp = redisTemplate.opsForHash();
+		hashOp.putAll(key, map);
+	}
+
+	public <T> void putCiLongComponentFromCache(String key, Map<Long, T> map){
+		HashOperations<String, Long, T> hashOp = redisTemplate.opsForHash();
+		hashOp.putAll(key, map);
 	}
 
 }
