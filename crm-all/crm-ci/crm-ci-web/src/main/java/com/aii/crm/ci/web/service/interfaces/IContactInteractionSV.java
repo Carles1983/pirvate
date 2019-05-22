a2dbca2c5ce8fe0a5c85d88faa2d967225da9557
Carles1983 <soane1983@163.com>
2019-05-22 16:39:21 +0800

    Update IContactInteractionSV.java

--- crm-all/crm-ci/crm-ci-web/src/main/java/com/aii/crm/ci/web/service/interfaces/IContactInteractionSV.java
+++ crm-all/crm-ci/crm-ci-web/src/main/java/com/aii/crm/ci/web/service/interfaces/IContactInteractionSV.java
@@ -2,8 +2,10 @@ package com.aii.crm.ci.web.service.interfaces;
 
 import com.aii.crm.ci.web.dto.req.CiInteractionReqDto;
 import com.aii.crm.common.exception.CrmCheckedException;
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
 
 public interface IContactInteractionSV {
-	void createContact(CiInteractionReqDto interactionReqDto) throws CrmCheckedException;
-	void createInteraction(CiInteractionReqDto interactionReqDto) throws CrmCheckedException;
+	boolean createContact(CiInteractionReqDto interactionReqDto) throws CrmCheckedException, ParseException;
+	void createInteraction(CiInteractionReqDto interactionReqDto) throws CrmCheckedException, ParseException, IllegalAccessException, InvocationTargetException, InstantiationException;
 }
