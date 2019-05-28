016528bcb8e0557167e21130932da4c98d5a2dba
Carles1983 <soane1983@163.com>
2019-05-28 16:13:17 +0800

    Update PreferChannelResDto.java

--- crm-all/crm-cm/crm-cm-web/src/main/java/com/aii/crm/cm/web/dto/res/PreferChannelResDto.java
+++ crm-all/crm-cm/crm-cm-web/src/main/java/com/aii/crm/cm/web/dto/res/PreferChannelResDto.java
@@ -1,4 +1,27 @@
 package com.aii.crm.cm.web.dto.res;
 
-public class PreferChannelResDto {
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class PreferChannelResDto implements Serializable {
+	private Long preferChannelId;
+	private Long custContRelaId;
+	private Long custId;
+	private Long perferType;
+	private String preferChannelName;
+	private Long prior;
+	private String state;
+	private Long createOpId;
+	private Long createOrgId;
+	private Long opId;
+	private Long orgId;
+	private Long doneCode;
+	private Date createDate;
+	private Date doneDate;
+	private Date effectiveDate;
+	private Date expireDate;
+	private String regionId;
+	private String remarks;
 }
