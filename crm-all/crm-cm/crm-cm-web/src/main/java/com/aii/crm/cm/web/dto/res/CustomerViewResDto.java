54890956d250b31c2c7b36cf7e6e82999ebda273
Carles1983 <soane1983@163.com>
2019-05-28 16:13:08 +0800

    Update CustomerViewResDto.java

--- crm-all/crm-cm/crm-cm-web/src/main/java/com/aii/crm/cm/web/dto/res/CustomerViewResDto.java
+++ crm-all/crm-cm/crm-cm-web/src/main/java/com/aii/crm/cm/web/dto/res/CustomerViewResDto.java
@@ -1,4 +1,26 @@
 package com.aii.crm.cm.web.dto.res;
 
-public class CustomerViewResDto {
+import java.io.Serializable;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class CustomerViewResDto implements Serializable {
+	//address info
+	private List<AddressViewResDto> addressList;
+	//contact info
+	private List<AddressViewResDto> contactList;
+	//basic info
+	private Long cust_id;
+	private Integer cust_cert_type;
+	private String cust_cert_code;
+	private String first_name;
+	private String middle_name;
+	private String last_name;
+	private Integer gender;
+	private String birthday;
+	private Integer cust_status;
+	private String cust_language;
+	private String remarks;
+	private String cust_name;
 }
