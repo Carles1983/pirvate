b196ef1f098b11b81e2ec847102064ae861b1d98
Carles1983 <soane1983@163.com>
2019-05-28 16:13:10 +0800

    Update ElectronMediumResDto.java

--- crm-all/crm-cm/crm-cm-web/src/main/java/com/aii/crm/cm/web/dto/res/ElectronMediumResDto.java
+++ crm-all/crm-cm/crm-cm-web/src/main/java/com/aii/crm/cm/web/dto/res/ElectronMediumResDto.java
@@ -1,4 +1,15 @@
 package com.aii.crm.cm.web.dto.res;
 
-public class ElectronMediumResDto {
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+public class ElectronMediumResDto implements Serializable {
+	private Long contactRelaId;
+	private Long custId;
+	private Long contactType;
+	private String contactValue;
+	private Long priority;
+	private Long disturbFlag;
+	private String remarks;
 }
