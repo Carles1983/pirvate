e913862da8e60123cbb1c765047a7e2b729d8283
Carles1983 <soane1983@163.com>
2019-05-28 16:13:06 +0800

    Update CustomerInfoResDto.java

--- crm-all/crm-cm/crm-cm-web/src/main/java/com/aii/crm/cm/web/dto/res/CustomerInfoResDto.java
+++ crm-all/crm-cm/crm-cm-web/src/main/java/com/aii/crm/cm/web/dto/res/CustomerInfoResDto.java
@@ -1,4 +1,20 @@
 package com.aii.crm.cm.web.dto.res;
 
-public class CustomerInfoResDto {
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+public class CustomerInfoResDto implements Serializable {
+	private Long custId;
+	private String email;
+	private Integer custType;
+	private String custName;
+	private Integer custCertType;
+	private String custCertCode;
+	private Integer custStatus;
+	private Integer gender;
+	private String legalAddress;
+	private String birthday;
+	private String firstName;
+	private String lastName;
 }
