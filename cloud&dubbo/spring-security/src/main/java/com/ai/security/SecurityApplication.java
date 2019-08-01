76124c02a62581992132046db89369c77682b107
Carles1983 <soane1983@163.com>
2019-08-01 16:35:20 +0800

    Create SecurityApplication.java

---
+++ cloud&dubbo/spring-security/src/main/java/com/ai/security/SecurityApplication.java
@@ -0,0 +1,18 @@
+package com.ai.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: soane
+ * @Date: 2019/7/30
+ * @Time: 11:07
+ * @Description: TODO
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+}
