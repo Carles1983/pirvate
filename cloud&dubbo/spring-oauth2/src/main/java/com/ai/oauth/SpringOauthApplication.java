b6e25568d149bcf7dda73a70b7189edfa31cf824
Carles1983 <soane1983@163.com>
2019-08-01 16:34:55 +0800

    Create SpringOauthApplication.java

---
+++ cloud&dubbo/spring-oauth2/src/main/java/com/ai/oauth/SpringOauthApplication.java
@@ -0,0 +1,18 @@
+package com.ai.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: soane
+ * @Date: 2019/8/1
+ * @Time: 9:47
+ * @Description: TODO
+ */
+@SpringBootApplication
+public class SpringOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthApplication.class, args);
+	}
+}
